eaa0777b1f269927ed47ddfd4dca4b5095f710c0
Mike Johanson <michael.johanson@intel.com>
2023-11-27 16:52:10 -0700

    fix: package.json dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
