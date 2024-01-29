c4adeee72c3bf0bb4acc6d4609f2cb0085aac7ee
Mike Johanson <michael.johanson@intel.com>
2024-01-29 13:28:41 -0700

    refactor: reduce any types

--- sol/src/terminal/terminal.component.ts
+++ sol/src/terminal/terminal.component.ts
@@ -4,41 +4,50 @@
  **********************************************************************/
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core'
 import { C, V } from '@angular/cdk/keycodes'
+import { Terminal } from 'xterm'
 @Component({
   selector: 'amt-terminal',
   templateUrl: './terminal.component.html'
 })
 
 export class TerminalComponent implements OnInit {
-  @Input() term: any
+  @Input() term: Terminal
   @Output() handleKeyPress: EventEmitter<any> = new EventEmitter<any>()
-  container!: any
+  container!: HTMLElement | null
   ngOnInit (): void {
     this.container = document.getElementById('terminal')
+    if (this.container == null) throw new Error('Container not found')
     this.term.open(this.container)
     this.term.onData((data: any) => {
       this.handleKeyPress.emit(data)
     })
-    this.term.attachCustomKeyEventHandler(async (e: any) => {
+    this.term.attachCustomKeyEventHandler((e: KeyboardEvent): boolean => {
       e.stopPropagation()
       // Due to a new 'HACK' in xtermjs, calling e.preventDefault() here
       // results in all upper case characters being dropped,
       // so only call it if we 'consume' the keydown here.
       // Note: this function can be called for 'keypress' and 'keyup' events as well as 'keydown' events
       if (e.type === 'keydown') {
-        if (e.ctrlKey === true && e.shiftKey === true && e.keyCode === C) {
+        if (e.ctrlKey && e.shiftKey && e.keyCode === C) {
           e.preventDefault()
-          await navigator.clipboard.writeText(this.term.getSelection())
-        } else if (e.ctrlKey === true && e.shiftKey === true && e.keyCode === V) {
+          navigator.clipboard.writeText(this.term.getSelection()).then(() => {
+
+          }).catch(err => {
+            console.error('Failed to copy to clipboard', err)
+          })
+        } else if (e.ctrlKey && e.shiftKey && e.keyCode === V) {
           e.preventDefault()
-          await navigator.clipboard.readText().then(text => {
+          navigator.clipboard.readText().then(text => {
             this.handleKeyPress.emit(text)
+          }).catch(err => {
+            console.error('Failed to read clipboard', err)
           })
         } else if (e.code === 'Space') { // or e.keyCode === SPACE
           e.preventDefault()
           this.handleKeyPress.emit(e.key)
         }
       }
+      return false
     })
   }
 }
